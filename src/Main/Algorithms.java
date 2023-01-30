8158bdf7d4c1c16b05f152e229bd666e335e7b5b
myne145 <antoni.kucz@gmail.com>
2023-01-30 14:09:45 +0100

    4.2 kinda ez

--- src/Main/Algorithms.java
+++ src/Main/Algorithms.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public abstract class Algorithms {
     public static ArrayList<String> loadFileArray(File file) throws IOException {
@@ -16,5 +17,18 @@ public abstract class Algorithms {
         for(String s : arr)
             stringBuilder.append(s);
         return stringBuilder.toString();
+    }
+
+    public static ArrayList<Integer> getHighestNumAndItsIndexInArr(ArrayList<Integer> arr) {
+        int temp = 0;
+        int tempIndex = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr.get(i) > temp) {
+                temp = arr.get(i);
+                tempIndex = i;
+            }
+        }
+
+        return new ArrayList<>(Arrays.asList(temp, tempIndex));
     }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -19,7 +19,26 @@ public class Main {
         }
         System.out.println(wordlength);
     }
+
+    private static void zad4Part2() throws IOException {
+        ArrayList<String> content = loadFileArray(new File("przyklad.txt"));
+        ArrayList<Integer> counters = new ArrayList<>();
+        int maxCount = 1;
+        for(int i = 1; i < content.size(); i++) {
+            if(content.get(i-1).split(" ")[0].equals(content.get(i).split(" ")[0])) {
+                maxCount++;
+                counters.add(maxCount);
+            }
+            else {
+                maxCount = 1;
+                counters.add(1);
+            }
+        }
+
+        ArrayList<Integer> result = getHighestNumAndItsIndexInArr(counters);
+        System.out.println("Liczba " + result.get(0) + " jest największa dla instrukcji: " + content.get(result.get(1)).split(" ")[0]);
+    }
     public static void main(String[] args) throws IOException {
-        zad4Part1();
+        zad4Part2();
     }
 }
